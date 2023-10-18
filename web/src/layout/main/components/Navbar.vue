c24844d8503e65304fdcbfa9802855548bbc092c
wzg_work <2978968560@qq.com>
2023-10-18 16:18:52 +0800

    完善头部

--- web/src/layout/main/components/Navbar.vue
+++ web/src/layout/main/components/Navbar.vue
@@ -3,11 +3,17 @@
     class="navbar-cp-container">
     <div class="left">
         <el-image 
+            @click="toPath({path:'/'})"
             class="logo"
             :src="'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'" fit="cover" />
+        <div class="name">
+            毒蘑菇 - 管理
+        </div>
     </div>
     <div class="right">
-        <div class="bt">
+        <div
+            @click="toggleFullScreen" 
+            class="bt">
             <svg t="1697598663799" 
                 class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4016" 
                 width="25" height="25"><path d="M170.666667 170.666667v213.333333H85.333333V85.333333h298.666667v85.333334H170.666667z m682.666666 213.333333V170.666667h-213.333333V85.333333h298.666667v298.666667h-85.333334zM170.666667 640v213.333333h213.333333v85.333334H85.333333v-298.666667h85.333334z m682.666666 0h85.333334v298.666667h-298.666667v-85.333334h213.333333v-213.333333z" fill="#2E2F30" p-id="4017"></path></svg>
@@ -22,13 +28,32 @@
             <div class="option">
                 <svg t="1697599063633" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="6297" width="32" height="32"><path d="M585.246075 875.340272l408.227201-447.966132c57.440818-63.040395 12.824837-164.374669-72.613865-164.374669H104.224378c-85.25807 0-130.054683 101.334274-72.613865 164.374669l408.407832 447.7855c39.016405 42.809667 106.211325 42.809667 145.22773 0.180632z" fill="#333333" p-id="6298"></path></svg>
             </div>
+            <div class="bt-list-container">
+                <div class="item">
+                    <router-link 
+                        to="/main/mine?activeTab=0">
+                        个人中心
+                    </router-link>
+                </div>
+                <div class="item">
+                    <router-link 
+                        to="/main/mine?activeTab=2">
+                        密码修改
+                    </router-link>
+                </div>
+                <div 
+                    class="logout-bt"
+                    @click.stop="onLogout">
+                    退出登录
+                </div>
+            </div>
         </div>
     </div>
 </div>
 </template>
 <script>
 /*
- * 标签切换按钮组件
+ * 头部组件
  * 提供类名供外部调整
  */
 import { 
@@ -36,93 +61,58 @@ import {
     computed,onMounted,watch,toRef,
     onUnmounted,
 } from "vue";
+import { useRouter } from "vue-router";
 
 export default {
-    name: 'MyTabs',
+    name: 'Navbar',
     props:{
-        activeIndex:{  //当前选择的index
-            type:Number,
-            default:0,
-        },
-        tabLength:{  //可切换的tab length
-            type:Number,
-            default:0,
-        },
-        gridStyle:{  //布局样式，等宽或自动
-            type:String,
-            default:'equal',
-        },
+        
     },
     setup(props){
-        const MyTabsRef = ref(null);
-        const ItemBgRef = ref(null);
+        const router = useRouter();
         const dataContainer = reactive({
-            show:false,
-            timer:'',
-            activeIndex:toRef(props,'activeIndex'),
-            tabLength:toRef(props,'tabLength'),
-            gridStyle:toRef(props,'gridStyle'),
+            
         });
-        /** 是否活跃 */
-        function isActive(){
-            const el = MyTabsRef.value;
-            if(!el) return false;
-            let react = el.getBoundingClientRect();
-            /** 元素被隐藏了也不计算 */
-            if(!react.width && !react.height) return false;
-            return el.getRootNode() === document;
-        }
-        /** 计算位置 */
-        function computedLocation(){ 
-            const MyTabsEl = MyTabsRef.value;
-            if(!MyTabsEl) return;
-            if(!isActive()) return;
-            const ItemEls = new Array(...MyTabsEl.children);
-            if(!ItemEls) return;
-            ItemEls.pop();
-            dataContainer.show = ItemEls.length > 0;
-            const ItemBgEl = ItemBgRef.value;
-            if(!ItemBgEl) return;
-            if(dataContainer.activeIndex<0 || dataContainer.activeIndex>=ItemEls.length) return;
-            let left = ItemEls[dataContainer.activeIndex].offsetLeft;
-            let top = ItemEls[dataContainer.activeIndex].offsetTop;
-            let transform = `translate(${left}px, ${top}px)`;
-            let width = ItemEls[dataContainer.activeIndex].clientWidth + 'px';
-            let height = ItemEls[dataContainer.activeIndex].clientHeight + 'px';
-            if(ItemBgEl.style.transform != transform){
-                ItemBgEl.style.transform = transform;
-            }
-            if(ItemBgEl.style.width != width){
-                ItemBgEl.style.width = width;
-            }
-            if(ItemBgEl.style.height != height){
-                ItemBgEl.style.height = height;
+        /** 全屏事件 */
+        function toggleFullScreen() {
+            var elem = document.documentElement; // 获取文档根元素
+            if (!document.fullscreenElement && // 当前不在全屏模式
+                !document.mozFullScreenElement && 
+                !document.webkitFullscreenElement && 
+                !document.msFullscreenElement) {  // 也适用于IE/Edge
+                if (elem.requestFullscreen) {
+                elem.requestFullscreen();
+                } else if (elem.mozRequestFullScreen) { // Firefox
+                elem.mozRequestFullScreen();
+                } else if (elem.webkitRequestFullscreen) { // Chrome, Safari, and Opera
+                elem.webkitRequestFullscreen();
+                } else if (elem.msRequestFullscreen) { // IE/Edge
+                elem.msRequestFullscreen();
+                }
+            } else { // 当前在全屏模式，退出全屏
+                if (document.exitFullscreen) {
+                document.exitFullscreen();
+                } else if (document.mozCancelFullScreen) { // Firefox
+                document.mozCancelFullScreen();
+                } else if (document.webkitExitFullscreen) { // Chrome, Safari, and Opera
+                document.webkitExitFullscreen();
+                } else if (document.msExitFullscreen) { // IE/Edge
+                document.msExitFullscreen();
+                }
             }
         }
-        watch(props,()=>{
-            computedLocation();
-        });
-        onMounted(()=>{
-            computedLocation();
-            /** 监听元素宽高变化 */
-            let timer;
-            window.addEventListener("resize",()=>{
-                clearTimeout(timer);
-                timer = setTimeout(()=>{
-                    computedLocation();
-                },150);
-            });
-            dataContainer.timer = setInterval(()=>{
-                computedLocation();
-            },150);
-        });
-        onUnmounted(()=>{
-            clearInterval(dataContainer.timer);
-        });
+        /** 跳转去相应页面 */
+        function toPath(params){
+            router.push(params);
+        }
+        /** 退出登录 */
+        function onLogout() {
+        }
         return {
-            MyTabsRef,
-            ItemBgRef,
             dataContainer,
+            toPath,
+            toggleFullScreen,
+            onLogout,
         };
     },
 }
@@ -146,6 +136,12 @@ export default {
             width: 150px;
             height: 50px;
             border-radius: 5px;
+            cursor: pointer;
+            margin-right: 20px;
+        }
+        >.name{
+            font-size: 20px;
+            font-weight: bold;
         }
     }
     >.right{
@@ -174,18 +170,19 @@ export default {
             cursor: pointer;
             border-radius: 5px;
             transition: all 0.2s;
+            position: relative;
             &:hover{
-                background-color: rgb(224, 224, 224);
+                background-color: rgba(194, 224, 255, 0.5);
             }
             >.img{
                 width: 40px;
                 height: 40px;
                 border-radius: 5px;
-                margin-right: 5px;
+                margin-right: 10px;
             }
             >.name{
                 font-size: 15px;
-                margin-right: 5px;
+                margin-right: 10px;
             }
             >.option{
                 width: fit-content;
@@ -198,6 +195,64 @@ export default {
                     height: 10px;
                     width: 10px;
                     vertical-align: bottom;
+                }
+            }
+            >.bt-list-container{
+                width: fit-content;
+                min-width: 150px;
+                position: absolute;
+                z-index: 9;
+                top: calc(100% + 0px);
+                right: 0;
+                background-color: rgb(255, 255, 255);
+                box-shadow: 0 3px 8px 0 rgba(0, 0, 0, 0.5);
+                padding: 10px 0;
+                box-sizing: border-box;
+                border-radius: 3px;
+                overflow: hidden;
+                opacity: 0;
+                transition: opacity 0.2s;
+                pointer-events: none;
+                font-size: 15px;
+                &:hover{
+                    opacity: 1;
+                    pointer-events: initial;
+                }
+                >.item{
+                    width: auto;
+                    min-width: max-content;
+                    transition: all 0.2s;
+                    &:hover{
+                        background-color: rgba(194, 224, 255, 0.5);
+                        :deep(a){
+                            color: #0072E5;
+                        }
+                    }
+                    :deep(a){
+                        padding: 13px 15px;
+                        box-sizing: border-box;
+                        display: block;
+                        color: #6b7386;
+                        text-align: center;
+                    }
+                }
+                >.logout-bt{
+                    cursor: pointer;
+                    padding: 13px 15px;
+                    box-sizing: border-box;
+                    background-color: #fef0f0;
+                    color: #f56c6c;
+                    transition: all 0.2s;
+                    text-align: center;
+                    &:hover{
+                        color: #f14242;
+                    }
+                }
+            }
+            &:hover{
+                .bt-list-container{
+                    opacity: 1;
+                    pointer-events: initial;
                 }
             }
         }
--- web/src/layout/main/components/TagList.vue
+++ web/src/layout/main/components/TagList.vue
@@ -9,7 +9,10 @@
                 <div
                     v-for="item,index in 50" 
                     :key="index"
-                    class="item">
+                    :class="{
+                        'item':true,
+                        'active':index==1,
+                    }">
                     标签 {{ index}}
                     <div class="bt">
                         <svg t="1697599667788" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="9077" width="32" height="32"><path d="M639.125 512l217.96875-217.96875c14.4375-14.4375 14.4375-40.03125 0-54.46875l-72.65625-72.65625c-14.4375-14.4375-40.125-14.4375-54.46875 0L512 384.78125 294.03125 166.8125c-14.4375-14.4375-40.125-14.4375-54.46875 0l-72.65625 72.65625c-14.4375 14.4375-14.4375 40.125 0 54.46875L384.78125 512 166.8125 729.96875c-14.4375 14.4375-14.4375 40.125 0 54.46875l72.65625 72.65625c14.4375 14.4375 40.125 14.4375 54.46875 0L512 639.125l217.96875 217.96875c14.4375 14.4375 40.125 14.4375 54.46875 0l72.65625-72.65625c14.4375-14.4375 14.4375-40.03125 0-54.46875L639.125 512z" p-id="9078"></path></svg>
@@ -17,6 +20,21 @@
                 </div>
             </div>
         </el-scrollbar>
+    </div>
+    <div class="right">
+        <div
+            @click="toggleFullScreen" 
+            class="bt">
+            <svg t="1697616141094" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="12019" width="32" height="32"><path d="M867.126867 516.953333l-416-416A52.986667 52.986667 0 0 0 413.413533 85.333333H53.333533a53.393333 53.393333 0 0 0-53.333333 53.333334v360.08a52.986667 52.986667 0 0 0 15.62 37.713333l416 416a53.333333 53.333333 0 0 0 75.426667 0l360.08-360.08a53.4 53.4 0 0 0 0-75.426667zM341.333533 341.333333a85.333333 85.333333 0 1 1-85.333333-85.333333 85.426667 85.426667 0 0 1 85.333333 85.333333z m653.793334 251.046667l-382.706667 382.706667a21.333333 21.333333 0 0 1-30.173333-30.173334l382.706666-382.706666a10.666667 10.666667 0 0 0 0-15.08L539.5802 121.753333a21.333333 21.333333 0 0 1 30.173333-30.173333l425.373334 425.373333a53.4 53.4 0 0 1 0 75.426667z" fill="#5C5C66" p-id="12020"></path></svg>
+        </div>
+        <div class="bt-list-container">
+            <div class="item">
+                关闭当前
+            </div>
+            <div class="item">
+                关闭其他
+            </div>
+        </div>
     </div>
 </div>
 </template>
@@ -147,13 +165,18 @@ export default {
                 justify-content: center;
                 align-items: center;
                 background-color: rgb(233, 233, 233);
-                padding: 5px;
+                padding: 5px 10px;
                 box-sizing: border-box;
                 border:1px solid rgb(196, 196, 196);
                 margin-left: 5px;
                 font-size: 13px;
                 height: 30px;
                 width: max-content;
+                &.active{
+                    background-color: #0072E5;
+                    color: rgb(255, 255, 255);
+                    font-weight: bold;
+                }
                 >.bt{
                     width: fit-content;
                     height: fit-content;
@@ -165,9 +188,76 @@ export default {
                         height: 15px;
                         width: 15px;
                         vertical-align: bottom;
-                        margin-left: 5px;
+                        margin-left: 10px;
                     }
                 }
+            }
+        }
+    }
+    >.right{
+        width: 50px;
+        height: 100%;
+        border-left: 1px solid rgb(218, 218, 218);
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        position: relative;
+        >.bt{
+            width: 100%;
+            height: 100%;
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: center;
+            cursor: pointer;
+            >svg{
+                height: 25px;
+                width: 25px;
+                vertical-align: bottom;
+            }
+        }
+        >.bt-list-container{
+            width: fit-content;
+            min-width: 150px;
+            position: absolute;
+            z-index: 9;
+            top: calc(100% + 0px);
+            right: 0;
+            background-color: rgb(255, 255, 255);
+            box-shadow: 0 3px 8px 0 rgba(0, 0, 0, 0.5);
+            padding: 10px 0;
+            box-sizing: border-box;
+            border-radius: 3px;
+            overflow: hidden;
+            opacity: 0;
+            transition: opacity 0.2s;
+            pointer-events: none;
+            font-size: 15px;
+            &:hover{
+                opacity: 1;
+                pointer-events: initial;
+            }
+            >.item{
+                cursor: pointer;
+                width: auto;
+                min-width: max-content;
+                transition: all 0.2s;
+                padding: 13px 15px;
+                box-sizing: border-box;
+                display: block;
+                color: #6b7386;
+                text-align: center;
+                &:hover{
+                    background-color: rgba(194, 224, 255, 0.5);
+                    color: #0072E5;
+                }
+            }
+        }
+        &:hover{
+            .bt-list-container{
+                opacity: 1;
+                pointer-events: initial;
             }
         }
     }
